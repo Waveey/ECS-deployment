6f0673f80ba5d0676ee35fc5e8063cbf8392033b
Waveey <ogonnannamani11@gmail.com>
2023-12-03 11:53:41 +0100

    AUTO-push

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the NEW-PAGE");
+  res.send("Welcome to the AUTOMATED-COPY");
 });
 
 app.get("/info", (req, res) => {
